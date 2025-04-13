b3ea01c5a49b8f594519a0ba9cb4c4456b2b37ee
mora-cat <maria.mora.git@gmail.com>
2025-04-13 02:01:15 -0500

    Completed and debugged all of part 1

--- part1-conditionals.js
+++ part1-conditionals.js
@@ -1,23 +1,31 @@
-/* LAB:
-Part 1: Conditionals (in part1-conditionals.js)
-1. Grade Checker 
-let score = 82;
-Print A, B, C, D, or F based on score */
-
-// --------MINI LESSON 🤔------------- 
-
+// WEEK 4 JAVASCRIPT LAB 2:
+// ---Part 1: Conditionals (in part1-conditionals.js) ---
+//
+//      1. GRADE CHECKER
+//      let score = 82;
+//      Print A, B, C, D, or F based on score */
+//
+// --------🐦 CONDITIONALS MINI LESSON 🐦------------- 
+//
 /* this variable holds the score we want to check: 82.
     _the word LET tells JS I'm declaring a variable.
     _the word SCORE is the name of my variable in this example.
     _VARIABLES can be named almost anything.
-    _the EQUAL SIGN = assigns the VALUE 82 to the variable score, I can change the value to another number. FOR NOW EVERYTIME I USE SCORE IN MY CODE IT WILL REPRESENT THE VALUE 82 !!! But if i use a FUNCTION it will let me check ANY score.  */
+    _the EQUAL SIGN = assigns the VALUE 82 to the variable score, I can change the value to another number. FOR NOW EVERY TIME I USE SCORE IN MY CODE IT WILL REPRESENT THE VALUE 82 !!! But if i use a FUNCTION it will let 
+    me check ANY score.  */
 
-/* HINT pay attention to the use of the ELSE in the last line:
+/* 🐞 HINT pay attention to the use of the ELSE in the last line:
 
             else {
             }
 
-    the else {} block is used as a "default action" like saying if all other, or any other, conditions are false this is what happens. In this way you dont go writing tons of code for every thinkable occasion 😂😂😂    */
+    this EMPTY else {}, because it has no variable name with a value,  is used as a "default action" like saying if all other, or any other, conditions are false this is what happens, in this example you get an F grade. In this way you don't go writing tons of code for every thinkable occasion 😂😂😂    */
+
+    //------------------🌻 END 🌻-----------------------
+
+// 📌   1. GRADE CHECKER
+//      let score = 82;
+//      Print A, B, C, D, or F based on score
 
 let score = 82; 
 if (score >= 90) {
@@ -31,5 +39,31 @@ if (score >= 90) {
 } else {
     console.log("F");
     // this ELSE {} empty, means if score is not A,B,C,D, then it is F.
+}
+
+    //  ------------------------------------------------
+
+// 📌   2. Pizza Quantity Warning 
+//      let quantity = 12;
+//      Print warning if quantity > 10
+
+let quantity = 12;
+if (quantity > 10){
+    console.log("Warning, there are more than 10 pizzas now");
+} else {
+     console.log ("All good, pizza quantity 10 or less");
+}
+   
+    //  ------------------------------------------------
+    
+// 📌   3. Login Simulator
+//      let password = "letmein"; // or use prompt
+//      Print "Access granted" or "Access denied"
+
+let password = "letmein";
+if (password === "letmein"){
+    console.log("Access granted");
+} else {
+    console.log("Access denied");
 }
 
