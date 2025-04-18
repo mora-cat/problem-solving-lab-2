a877af6fc3860bbfc09ac442605c65af0779ca85
mora-cat <maria.mora.git@gmail.com>
2025-04-18 03:49:09 -0500

    finished the part3-interactions.js

---
+++ part3-interaction.js
@@ -0,0 +1,17 @@
+//
+// to change the message when you click the greet button
+
+function sayHello() {
+    //in html file, find the <p> with id = "message" and change the text inside it:
+    document.getElementById("message").textContent = "Hola 😎 mundo del web!";
+}
+
+// this function runs when you click the Submit button 
+
+function showName() {
+    let name = document.getElementById("nameInput").value;
+    // find the <p> with the id = "output"and put the name there
+    document.getElementById("output").textContent = `Nice to meet you, ${name}!`;
+}
+
+
